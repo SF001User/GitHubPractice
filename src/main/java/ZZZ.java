b6a9b45b160c255baa993da24137dd65a89aacda
EU3 Batch <fedbidtest1@gmail.com>
2020-09-28 23:20:14 -0400

    GOING BACK

---
+++ src/main/java/ZZZ.java
@@ -0,0 +1,2 @@
+public class ZZZ {
+}
---
+++ src/test/java/HelloX.java
@@ -0,0 +1,9 @@
+public class HelloX {
+
+    public static void main(String[] args) {
+
+        System.out.println("args = ");
+        System.out.println("more stuff here");
+    }
+
+}
